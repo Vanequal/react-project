882c759c526fd1d66682d688e6872f55f590a811
Vanequal <adgarovzafar21@gmail.com>
2025-04-05 18:21:13 +0500

    routing

--- src/pages/ConfirmSending.tsx
+++ src/pages/ConfirmSending.tsx
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import ConfirmCard from '../components/UI/ConfirmCard';
 import ProfileCheckCard from '../components/UI/ProfileCheckCard';
+import Button from '../components/UI/Button';
 import ButtonInline from '../components/UI/ButtonInline';
 import TelegramAnimation from '../components/UI/TelegramAnimation';
-import DLCBadge from '../components/UI/DLCBadge';
-import ActivationTimer from '../components/UI/ActivationTimer';
 import logo from '../assets/logo-main.svg';
 import desktopBackground from '../assets/DLC-background.png';
 import mobileBackground from '../assets/DLC-background-mobile.png';
-import avatar2 from '../assets/avatar-2.png';
-import styles from '../styles/ConfirmSending.module.scss';
+import gift from '../assets/gift.png';
+import giftAvatar from '../assets/gift-avatar.png';
+import styles from '../styles/CheckCode.module.scss';
 
 const ConfirmSending: React.FC = () => {
     const [isMobile, setIsMobile] = useState(false);
@@ -35,93 +35,81 @@ const ConfirmSending: React.FC = () => {
                         <div className={styles.headerLeft}>
                             <img src={logo} alt="Логотип" />
                         </div>
-                        <div className={styles.headerCenter}>
-                            <p>Для активации DLC нужна <br /> основная игра на аккаунте</p>
+                        <div className={styles.headerRight}>
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                <path d="M18 6L6 18" stroke="white" strokeWidth="2" strokeLinecap="round" />
+                                <path d="M6 6L18 18" stroke="white" strokeWidth="2" strokeLinecap="round" />
+                            </svg>
                         </div>
                     </div>
 
-                    {/* Мобильный контент */}
                     <div className={styles.mobileContent}>
                         <div className={styles.avatarWrapper}>
-                            <img src={avatar2} alt="Avatar" className={styles.avatar} />
-                        </div>
-                        <div className={styles.badgeTimerRow}>
-                            <DLCBadge />
-                            <ActivationTimer time="00:00:00" />
+                            <img src={gift} alt="Gift" className={styles.avatar} />
+                            <img src={giftAvatar} alt="Gift Avatar" className={styles.giftAvatarOverlay} />
                         </div>
                         <div className={styles.title}>
-                            <p>Command &amp; Conquer™ Red Alert™ 3- Uprising</p>
+                            <h1 style={{color: '#0DC11F'}}>Удачная покупка!</h1>
                         </div>
                         <div className={styles.orderInfo}>
-                            <p>Заказ #99999999</p>
-                            <p>
-                                Steam никнейм покупателя. Проверьте перед покупкой!
-                                <br />
-                                <a
-                                    href="https://steamcommunity.com/profiles/steamid64"
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    https://steamcommunity.com/profiles/steamid64 
-                                </a>  
-                                &nbsp; Вам отправлен запрос на добавление друзья в Steam. Вам необходимо принять нашего бота с никнеймом “Bot Name” в друзья. Далее вам <br /> отправят купленный товар.
-                        </p>
-
+                            <p>Спасибо за покупку товара в нашем магазине! <br /> Будем рады Вашему отзыву, обращайтесь ещё!</p>
+                        </div>
+                        <div className={styles.buttons}>
+                            <Button>Оставить отзыв</Button>
+                            <ButtonInline>Главная</ButtonInline>
+                            <a href="#" style={{color: 'white', textDecoration: 'none'}}>Посмотреть версию издания</a>
+                            <a href="#" style={{color: 'white', textDecoration: 'none'}}>Посмотреть игру</a>
+                        </div>
                     </div>
-                    <div className={styles.buttons}>
-                        <ButtonInline>Сменить аккаунт</ButtonInline>
-                        <a href="#" style={{color: 'white'}}>Связаться с продавцом</a>
-                    </div>
-                </div>
 
-            <div className={styles.mobileFooter}>
-                <div className={styles.langContainer}>
-                    <span
-                        className={`${styles.lang} ${lang === 'RU' ? styles.active : ''}`}
-                        onClick={() => setLang('RU')}
-                    >
-                        RU
-                    </span>
-                    <span
-                        className={`${styles.lang} ${lang === 'ENG' ? styles.active : ''}`}
-                        onClick={() => setLang('ENG')}
-                    >
-                        ENG
-                    </span>
-                </div>
-                <div className={styles.telegramAnimation}>
-                    <TelegramAnimation telegramLink="https://t.me/your_telegram" />
-                </div>
-            </div>
-        </>
-    ) : (
-        <>
-            <div className={styles.logo}>
-                <img src={logo} alt="Логотип" />
-            </div>
-            <div className={styles.cardsContainer}>
-                <ConfirmCard
-                    gameTitle="Command & Conquer™ Red Alert™ 3- Uprising"
-                    orderNumber="Заказ #99999999"
-                    timerTime="00:00:00"
-                />
-                <ProfileCheckCard
-                    headerText="Проверка профиля"
-                    profileUrl="https://steamcommunity.com/profiles/steamid64"
-                    buttoninlinetext="Сменить аккаунт"
-                    showMainButton={false}
-                    friendRequestMessage={true}
-                    showSellerLink={true}
-                    avatarSrc={avatar2}
-                    buttonMainText = ''
-                    steamNickname='Steam никнейм покупателя. Проверьте <br /> перед покупкой! <br />'
-                    showClose={false}
-                />
-            </div>
-        </>
-    )
-}
-        </div >
+                    <div className={styles.mobileFooter}>
+                        <div className={styles.langContainer}>
+                            <span
+                                className={`${styles.lang} ${lang === 'RU' ? styles.active : ''}`}
+                                onClick={() => setLang('RU')}
+                            >
+                                RU
+                            </span>
+                            <span
+                                className={`${styles.lang} ${lang === 'ENG' ? styles.active : ''}`}
+                                onClick={() => setLang('ENG')}
+                            >
+                                ENG
+                            </span>
+                        </div>
+                        <div className={styles.telegramAnimation}>
+                            <TelegramAnimation telegramLink="https://t.me/your_telegram" />
+                        </div>
+                    </div>
+                </>
+            ) : (
+                <>
+                    <div className={styles.logo}>
+                        <img src={logo} alt="Логотип" />
+                    </div>
+                    <div className={styles.cardsContainer}>
+                        <ConfirmCard
+                            gameTitle="Command & Conquer™ Red Alert™ 3- Uprising"
+                            orderNumber="Заказ #99999999"
+                            timerTime="00:00:00"
+                        />
+                        <ProfileCheckCard
+                            headerText="Удачная покупка"
+                            profileUrl=""
+                            buttoninlinetext="Главная"
+                            showMainButton={true}
+                            friendRequestMessage={false}
+                            showGame={true}
+                            showGameEdition={true}
+                            avatarSrc={gift}
+                            buttonMainText='Оставить отзыв'
+                            steamNickname='Спасибо за покупку товара в нашем магазине! <br /> Будем рады Вашему отзыву, обращайтесь ещё!'
+                            showClose={true}
+                        />
+                    </div>
+                </>
+            )}
+        </div>
     );
 };
 
